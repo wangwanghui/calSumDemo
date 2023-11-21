a9ca6693d174c29bf068b01122a17a2191049bbd
王圣辉 <1799119244@qq.com>
2023-11-21 22:50:55 +0800

    test jenkins

--- src/main/java/com/appleyk/demoController/TestController.java
+++ src/main/java/com/appleyk/demoController/TestController.java
@@ -1,14 +1,18 @@
 package com.appleyk.demoController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@Controller
+@RequestMapping("/test")
 public class TestController {
 
 
     @GetMapping("/jenkins")
+    @ResponseBody
     public String getHello(){
         return "HELLO-JENKINS SUCCESS";
     }
